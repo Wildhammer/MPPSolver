853b16de405a112e63fb37b979416b21d4822726
Mohammed Behbooei <behbooeid@yahoo.com>
2016-05-01 01:51:57 -0400

    ezsolver is implemented

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import math
 
 def schedule(paths,costs):
 	cumulative_cost = [[sum(k[i:]) if i!=len(k) else 0 for i in xrange(len(k)+1)] for k in costs]
-	print "cumulative_cost: %s" % cumulative_cost
+	# print "cumulative_cost: %s" % cumulative_cost
 	q = Q.PriorityQueue()
 	init_pos = tuple(0 for key in paths) 
 	#The data structure for priority queue is (f,current,parent,cost)
@@ -25,10 +25,10 @@ def schedule(paths,costs):
 		#print "pop: %s" % str(u)
 
 		if u[1] == goal:
-			return get_path(visited,goal)
+			return get_path_in_cez(visited,goal)
 		#all possible neighbors
 		all_neighbors=[(u[3]+costs[i][u[1][i]+j-1 if j==1 else u[1][i]+j]+sum([cumulative_cost[i][u[1][i]+j] if c==i else cumulative_cost[c][u[1][c]] for c in xrange(len(costs))]),u[1][:i]+(u[1][i]+j,)+u[1][i+1:],u[1],u[3]+costs[i][u[1][i]+j-1 if j==1 else u[1][i]+j]) for i in xrange(len(paths)) for j in [-1,1] if (u[1][i]+j>=0 and u[1][i]+j<len(paths[i])) ]
-		#filter visited neighbors out
+		#filtering out visited neighbors
 		new_neighbors=[(i,j,k,l) for (i,j,k,l) in all_neighbors if j not in [x[1] for x in visited] ]	
 
 		#removing collisions(illegals)
@@ -44,11 +44,9 @@ def schedule(paths,costs):
 				q.put(new_neighbors[i])	
 				visited.append(new_neighbors[i])
 
-
-
 	return []
 
-def get_path(visited,goal):
+def get_path_in_cez(visited,goal):
 	path = [goal]
 	curr = goal
 	start = tuple(0 for k in range(len(goal)))
@@ -58,38 +56,42 @@ def get_path(visited,goal):
 	path.reverse()
 	return path
 
-def ezsolver(graph,starts,goals):
-    (predecessor,distance) = floyd_warshall_predecessor_and_distance(graph, weight='weight')
+def get_path_in_floyd(start,goal,predecessor,distance):
+	path = [goal]
+	cost = []
+	curr = goal
+	while curr != start:
+		temp = predecessor[start][curr]
+		path.append(temp)
+		cost.append(distance[curr][temp])			
+		curr = temp
+	path.reverse()
+	cost.reverse()
+	return (path,cost)
 
-    return#a sequence of states should be returned
+def ezsolver(starts,goals,predecessor,distance):
+	res = [get_path_in_floyd(starts[i],goals[i],predecessor,distance) for i in xrange(len(starts))]
+	# print("res: ",res)
+	return schedule([i[0] for i in res],[i[1] for i in res])
+	 
+
 
 G=nx.Graph()
 
 G.add_edge(1,2,weight=5)
 G.add_edge(1,3,weight=2)
-G.add_edge(2,3,weight=2)
+G.add_edge(2,3,weight=9)
 G.add_edge(3,4,weight=1)
-G.add_edge(2,4,weight=3)
+G.add_edge(2,4,weight=1)
 G.add_edge(2,5,weight=4)
 G.add_edge(4,5,weight=2)
- 
-(predecessor,distance) = floyd_warshall_predecessor_and_distance(G, weight='weight')
-
-# print(predecessor[1])
-# esmall=[(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] <=0.5]
 
 # pos=nx.spring_layout(G,k=10/math.sqrt(G.order()),scale=2,iterations=100) # positions for all nodes
 pos={1:[1,1],2:[2,1],3:[1,2],4:[2,2],5:[3,1.5]}
 
-paths=[[1,3,4,5],[2,3,1]]
-costs=[[2,1,2],[2,2]]
+(predecessor,distance) = floyd_warshall_predecessor_and_distance(G, weight='weight')
+print "answer: %s" % ezsolver([1,5],[5,3],predecessor,distance)
 
-print "paths: %s" % paths
-print "costs: %s" % costs
-
-print "answer: %s" % schedule(paths,costs)
-# b = tuple(value[0] for key,value in pos.iteritems())
-# print b
 
 
 # nodes
